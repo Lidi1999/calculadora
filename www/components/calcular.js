0b2385591a707e2173c6e8d23b83357874ca6f84
Lidi1999 <lidiane.pires11@outlook.com>
2018-08-09 03:17:09 +0900

    Alteração do Index , Css e implementação de programação no botão somar

---
+++ www/components/calcular.js
@@ -0,0 +1,9 @@
+$(document). on ("click", "#somar", function(){
+        var valor1 = parseInt($("#valor1").val());
+        var valor2 = parseInt($("#valor2").val());
+        
+        var result = valor1 + valor2;
+        
+        $("#valor3").val(result);
+    });
+
