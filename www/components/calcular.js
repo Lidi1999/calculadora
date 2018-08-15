049e5ed1e12257721dfd0dff9bd7c0b8eb91b756
Lidi1999 <lidiane.pires11@outlook.com>
2018-08-16 03:18:36 +0900

    Adicionando alteração de index e css

--- www/components/calcular.js
+++ www/components/calcular.js
@@ -7,3 +7,31 @@ $(document). on ("click", "#somar", function(){
         $("#valor3").val(result);
     });
 
+$(document). on ("click", "#sub", function(){
+        var valor1 = parseInt($("#valor1").val());
+        var valor2 = parseInt($("#valor2").val());
+        
+        var result = valor1 - valor2;
+        
+        $("#valor3").val(result);
+    });
+    
+$(document). on ("click", "#mult", function(){
+        var valor1 = parseInt($("#valor1").val());
+        var valor2 = parseInt($("#valor2").val());
+        
+        var result = valor1 * valor2;
+        
+        $("#valor3").val(result);
+    });
+
+$(document). on ("click", "#div", function(){
+        var valor1 = parseInt($("#valor1").val());
+        var valor2 = parseInt($("#valor2").val());
+        
+        var result = valor1 / valor2;
+        
+        $("#valor3").val(result);
+    });
+
+
